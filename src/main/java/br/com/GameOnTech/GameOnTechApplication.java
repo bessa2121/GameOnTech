efe34932f2acf587c774aa6c3536ab116c1129d3
Davi Tavares <dxrktavares3@gmail.com>
2026-02-06 11:01:28 -0300

    feat: adiciona arquitetura base do projeto

---
+++ src/main/java/br/com/GameOnTech/GameOnTechApplication.java
@@ -0,0 +1,13 @@
+package br.com.GameOnTech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameOnTechApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GameOnTechApplication.class, args);
+	}
+
+}
