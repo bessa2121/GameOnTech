b233bb6226daffe0841d6a0874c0ca1a9f914cb6
Davi Tavares <dxrktavares3@gmail.com>
2026-02-09 19:26:04 -0300

    deploy inicial

--- src/test/java/br/com/GameOnTech/GameOnTechApplicationTests.java
+++ src/test/java/br/com/GameOnTech/GameOnTechApplicationTests.java
@@ -3,10 +3,10 @@ package br.com.GameOnTech;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class GameOnTechApplicationTests {
 
-	@Test
+	//@Test
 	void contextLoads() {
 	}
 
