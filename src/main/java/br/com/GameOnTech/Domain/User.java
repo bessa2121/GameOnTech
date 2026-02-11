117a6c0c5cf0e49005a4d557271902b60a8297dd
Davi Tavares <dxrktavares3@gmail.com>
2026-02-11 16:48:34 -0300

    fix: altera banco de mysql para postgresql e corrige script para mover abas de logs (pessoal e empresarial)

--- src/main/java/br/com/GameOnTech/Domain/User.java
+++ src/main/java/br/com/GameOnTech/Domain/User.java
@@ -25,7 +25,6 @@ public class User implements UserDetails {
 
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
-    @JdbcTypeCode(SqlTypes.CHAR)
     private UUID id;
 
     @Column(name = "email", unique = true, nullable = false)
--- src/main/resources/db.migration/V1__init.sql
+++ src/main/resources/db.migration/V1__init.sql
@@ -1,19 +1,21 @@
+CREATE TYPE user_role AS ENUM ('COLABORADOR', 'ADMIN');
+
 CREATE TABLE users (
-    id CHAR(36) PRIMARY KEY,
+    id UUID PRIMARY KEY,
     name VARCHAR(255) NOT NULL,
     email VARCHAR(255) UNIQUE NOT NULL,
     password VARCHAR(255) NOT NULL,
-    role ENUM('COLABORADOR', 'ADMIN') NOT NULL
+    role user_role NOT NULL
 );
 
 CREATE TABLE feedback (
-    id BIGINT PRIMARY KEY AUTO_INCREMENT,
+    id BIGSERIAL PRIMARY KEY,
     title VARCHAR(255) NOT NULL,
     message TEXT NOT NULL,
     response TEXT,
     status VARCHAR(20) NOT NULL DEFAULT 'ABERTO',
-    sender_id CHAR(36) NOT NULL,
-    receiver_id CHAR(36) NOT NULL,
+    sender_id UUID NOT NULL,
+    receiver_id UUID NOT NULL,
     CONSTRAINT fk_sender FOREIGN KEY (sender_id) REFERENCES users(id),
     CONSTRAINT fk_receiver FOREIGN KEY (receiver_id) REFERENCES users(id)
 );
